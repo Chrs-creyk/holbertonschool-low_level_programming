4b5baae5833f841e9fa53a3b20fff4104f5265f3
Chrs-creyk <chrisacount117@gmail.com>
2022-10-13 17:42:30 -0500

    combination of single num

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
+
 /**
  * main - numbers 0 -9
  * Return: 0
@@ -11,11 +10,16 @@ int main(void)
 
 	int a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		printf("%d, ", a);
-	}
-	putchar('\n');
+		putchar(a);
 
+		if (a != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar(10);
 	return (0);
 }
