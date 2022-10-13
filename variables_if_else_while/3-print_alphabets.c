93b5c79ce07e0e1188965205b7d3e67ee4407220
Chrs-creyk <chrisacount117@gmail.com>
2022-10-13 11:50:46 -0500

    lowecase and uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - printing an alphabet in lowercase and uppercase
+* Return: (0)
+*/
+
+int main(void)
+{
+char a, A;
+for (a = 'a' ; a <= 'z'; a++)
+{
+putchar(a);
+}
+
+for (A = 'A' ; A <= 'Z'; A++)
+{
+putchar(A);
+}
+
+putchar('\n');
+return (0);
+}
