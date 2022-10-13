69c3df651462b05f3a0b89d4404c29d6670d777e
Chrs-creyk <chrisacount117@gmail.com>
2022-10-12 23:50:19 -0500

    last N2

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,17 +9,19 @@
 int main(void)
 
 {
-	int n = rand() - RAND_MAX / 2;
+	int n;
 
 srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
+	if (n % 10 && n > 5)
 		printf("%d is positive\n", n);
 
-	if (n == 0)
+	if (n % 10 && n == 0)
 		printf("%d is zero\n", n);
 
-	if (n < 0)
+	if (n % 10 && n < 6)
 		printf("%d is negative\n", n);
+
 	return (0);
 }
