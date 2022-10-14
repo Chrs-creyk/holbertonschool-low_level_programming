6cb69d8bb3482c7cc76f2d8ede230e8aa6462430
Chrs-creyk <chrisacount117@gmail.com>
2022-10-14 17:42:12 -0500

    add of 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+/**
+ *add - adds two numbers for probllem
+ *
+ *@a: first number
+ *@b: second number
+ *
+ *Return: sum 2 numbers
+ */
+int add(int a, int b)
+{
+
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
