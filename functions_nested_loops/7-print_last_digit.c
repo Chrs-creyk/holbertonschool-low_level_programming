4cbd4d94588d33ceaefbf7239b4ac40a20313ef3
Chrs-creyk <chrisacount117@gmail.com>
2022-10-14 16:57:33 -0500

    the last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+
+#include "main.h"
+/**
+ * print_last_digit - print
+ * @n: result
+ * Return: the var
+*/
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+
+
+	if (n < 0)
+	{
+		last = last * -1;
+		_putchar(last + '0');
+	}
+
+		else
+		_putchar(last + '0');
+	return (last);
+}
