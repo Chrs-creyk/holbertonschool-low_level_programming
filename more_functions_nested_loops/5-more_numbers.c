ffdd6cff3f2d6cfbd20da6a4f541854035dec298
Chrs-creyk <chrisacount117@gmail.com>
2022-10-14 20:46:30 -0500

    adding 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - Prototy
+ * Return: no R
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b > 9)
+
+				_putchar(b / 10 + '0');
+
+			_putchar(b % 10 + '0');
+		}
+
+		_putchar('\n');
+	}
+}
