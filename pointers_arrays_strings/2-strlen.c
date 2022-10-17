63c9fb6e8c7476a1b5e95b80d695b5568da7ca01
Chrs-creyk <chrisacount117@gmail.com>
2022-10-17 18:09:42 -0500

    returning

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - the length of the string
+ * @s: pointer
+ * Return: x
+ */
+
+int _strlen(char *s)
+{
+	int x;
+
+	x = _strlen(s);
+
+	return (x);
+}
