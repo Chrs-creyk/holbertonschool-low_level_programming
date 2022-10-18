d0321013fae776a0bc071428a6cc04fecd910eb1
Chrs-creyk <chrisacount117@gmail.com>
2022-10-18 11:08:59 -0500

    reverses strings

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - Reverses a string
+ * @s: String to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int length, i;
+	char *begin_ptr, *end_ptr, ch;
+
+	length = strlen(s);
+	begin_ptr = s;
+	end_ptr = s;
+
+	for (i = 0; i < length - 1; i++)
+		end_ptr++;
+
+	for (i = 0; i < length / 2; i++)
+	{
+		ch = *end_ptr;
+		*end_ptr = *begin_ptr;
+		*begin_ptr = ch;
+
+		begin_ptr++;
+		end_ptr--;
+	}
+}
