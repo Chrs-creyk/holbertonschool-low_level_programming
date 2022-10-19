a07650f6d3faa10fab2097240d15a37a9e0aa35a
Chrs-creyk <chrisacount117@gmail.com>
2022-10-19 16:51:53 -0500

    adding the array are pointer and the copied string

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -5,17 +5,18 @@
  *@a: varible
  *@n: varible
  */
+
 void print_array(int *a, int n)
 {
+
 	int i;
 
-	for (; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (i == 0)
 			printf("%d", a[i]);
 		else
 			printf(", %d", a[i]);
 	}
-	if (n <= 4)
-		printf("\n");
+	printf("\n");
 }
