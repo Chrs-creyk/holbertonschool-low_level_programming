85cb83cbc659cdc6a0bd80a35100e73139f07bd6
Chrs-creyk <chrisacount117@gmail.com>
2022-10-20 17:47:04 -0500

    contacatenating 2 strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -13,14 +13,14 @@ char *_strcat(char *dest, char *src)
 	int len1 = 0, len2 = 0;
 	int i;
 
-	while (src[len1] <= src[len1 - 1])
+	while (src[len1] != '\0')
 		len1++;
 
-	while (dest[len2] <= dest[len2 - 1])
+	while (dest[len2] != '\0')
 		len2++;
 
 	for (; i <= len1; i++)
-		
+
 	{
 		dest[len2] = src[i];
 
