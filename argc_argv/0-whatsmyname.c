676b4c54743e990ccde11c050f3e955688c60f9b
chrs-creyk <chrisacount117@gmail.com>
2022-10-27 16:55:11 -0700

    my name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,19 +1,17 @@
 #include "main.h"
 /**
- * main - print the program name
- * @argc: count of arguments
- * @argv: count of string
- * Retunr 0
- */
-
+*main - print the program name
+*@argc: count of arguments
+*@argv: count of string
+*Retunr 0
+*/
 int main(int argc, char *argv[])
 {
+
 	if (argc > 0)
 	{
 		printf("%s\n", argv[0]);
 	}
-
 	return (0);
 }
-
 
