ecd4d7f02db6d61a7237642dad1173c282f42f5f
Chrs-creyk <chrisacount117@gmail.com>
2022-10-28 21:55:19 -0500

    improved

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints all arguments it receives
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int arg;
+
+	for (arg = 0; arg < argc; arg++)
+	{
+		printf("%s\n", argv[arg]);
+	}
+	return (0);
+}
