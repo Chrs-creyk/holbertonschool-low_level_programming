98b8f910f274af9568d1f997314eeb3b4cec544b
Chrs-creyk <chrisacount117@gmail.com>
2022-11-02 07:57:59 -0500

    malloc checked

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * malloc_checked - where the valor is saved
+ *@b: var
+ *Return: allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = 0;
+
+	ptr = malloc(sizeof(b));
+
+	if (i == 0)
+		exit(98)
+}
