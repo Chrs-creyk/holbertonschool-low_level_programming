ffb2cbb2b858bbc5a702a0c0df91c1a1b75a5c76
Chrs-creyk <chrisacount117@gmail.com>
2022-11-01 19:05:19 -0500

    a free grid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * free_grid - free the grid
+ * @grid: the grid
+ * @height: the columns
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a < height; a++)
+
+		free(grid[a]);
+
+	free(grid);
+}
