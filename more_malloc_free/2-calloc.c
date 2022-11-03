dbb8fa845165ee4a019c27189ea363cf5230b865
Chrs-creyk <chrisacount117@gmail.com>
2022-11-03 17:12:35 -0500

    arrays

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	l = nmemb + size;
+	l = nmemb * size;
 	p = malloc(l);
 	if (p == NULL)
 		return (NULL);
