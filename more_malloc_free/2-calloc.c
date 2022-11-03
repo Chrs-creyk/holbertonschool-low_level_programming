06aec551ed45f346ccbcbf613c58ead4444c8c32
Chrs-creyk <chrisacount117@gmail.com>
2022-11-03 17:09:07 -0500

    arrays

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -22,5 +22,19 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		p[i] = 0;
 		i++;
 	}
+int i = 0, l = 0;
+	char *p;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	l = nmemb * size;
+		p = malloc(l);
+	if (p == NULL)
+		return (NULL);
+	while (i < l)
+	{
+		p[i] = 0;
+		i++;
+	}
+	return ;
 }
