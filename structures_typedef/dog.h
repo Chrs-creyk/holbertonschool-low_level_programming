955ba510742131ad4dca54be84183a5816b9d32e
Chrs-creyk <chrisacount117@gmail.com>
2022-11-08 11:19:29 -0500

    first estructure called dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,22 @@
-#include <stdio>
+#ifndef _DOG_H_
+#define _DOG_H_
+#include <stdio.h>
+
+
+/**
+ * Struct_dog - a structure called dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: 0
+ *
+ */
 struct dog
 {
-    char name;
-    int age;
-    char owner;
-}
+
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
