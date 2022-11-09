772781026d321d3e907adc526c3130ec27d7e543
Chrs-creyk <chrisacount117@gmail.com>
2022-11-08 22:32:34 -0500

    initialize dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - function to initialize
+ * @d: pointer
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: null
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,7 +2,6 @@
 #define _DOG_H_
 #include <stdio.h>
 
-
 /**
  * struct dog - a structure called dog
  * @name: dog name
@@ -17,6 +16,11 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-
 };
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
