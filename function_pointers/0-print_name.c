51ba4b0f813df43cea4c6d23e6eddfab43692846
Chrs-creyk <chrisacount117@gmail.com>
2022-11-09 11:54:18 -0500

    funtion to pointer 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - function
+ * @name: a char
+ * @f: void
+ * Return: NULL
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		f(name);
+}
