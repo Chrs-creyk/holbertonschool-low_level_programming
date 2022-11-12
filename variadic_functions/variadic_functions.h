f8ae565806a82624fbac8e77657c6a2d322a8a0e
Chrs-creyk <chrisacount117@gmail.com>
2022-11-11 22:17:51 -0500

    sums all

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,4 +1,23 @@
+
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct format_types - struct format_types
+ * @identifier: conversion specifier
+ * @f: function pointer
+ */
+typedef struct format_types
+{
+	char *identifier;
+	void (*f)(char *separator, va_list args);
+} f_dt;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+void print_all(const char *const format, ...);
+
+#endif
