bd3232f27df3e901fdc3ed0e0e662994bdf6079b
Chrs-creyk <chrisacount117@gmail.com>
2022-11-13 18:26:23 -0500

    free dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - macro to free
+ * @d: pointer with the value
+ * return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+
+		free(d);
+	}
+}
