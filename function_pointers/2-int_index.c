b465108ae564b02fdc99c0c88c32aca34a3c82a1
Chrs-creyk <chrisacount117@gmail.com>
2022-11-20 22:44:09 -0500

    funtion pointers

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,23 +1,24 @@
-#include "variadic_functions.h"
+#include "function_pointers.h"
 /**
- * int_index - search for integer
- * @array: array
- * @size: size
- * @cmp_ 
- * @
+ * int_index - the function name
+ * @array:the array
+ * @size:length
+ * @cmp:the pointer to the function
+ * Return:-1 or ptr
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int y;
+    int elements, size1 = size;
 
     if (array && cmp)
     {
         if (size <= 0)
             return (-1);
 
-        for (y = 0; y < size; y++)
-            if (cmp(array[y]))
-                return (y);
+        for (elements = 0; elements < size1; elements++)
+            if (cmp(array[elements]))
+                return (elements);
     }
     return (-1);
 }
