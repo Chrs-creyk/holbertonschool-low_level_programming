668fcfbdce8c73a435a9cc8f2c875935e73b70a4
Chrs-creyk <chrisacount117@gmail.com>
2022-12-09 10:26:57 -0500

    free the dlist

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -5,31 +5,31 @@
  * @head: pointer const listint_t
  * @n: const integer
  * Return: prints all the elements of a list_t list.
-*/
+ */
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *point, *t;
+    dlistint_t *point, *t;
 
-	point = malloc(sizeof(dlistint_t));
-	if (point == NULL)
-		return (NULL);
+    point = malloc(sizeof(dlistint_t));
+    if (point == NULL)
+        return (NULL);
 
-	t = *head;
-	point->n = n;
-	point->next = NULL;
+    t = *head;
+    point->n = n;
+    point->next = NULL;
 
-	if (*head == NULL)
-	{
-		point->prev = NULL;
-		*head = point;
-	}
-	else
-	{
-		while (t->next)
-			t = t->next;
-		point->prev = t;
-		t->next = point;
-	}
-	return (point);
+    if (*head == NULL)
+    {
+        point->prev = NULL;
+        *head = point;
+    }
+    else
+    {
+        while (t->next)
+            t = t->next;
+        point->prev = t;
+        t->next = point;
+    }
+    return (point);
 }
